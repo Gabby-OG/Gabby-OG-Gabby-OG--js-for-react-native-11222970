d4342659acb860c8ea161b44dd8092857599821f
Gabriel <opokugabriel2004@gmail.com>
2024-05-18 21:33:36 +0000

    Second task

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -4,3 +4,17 @@ function processArray(arr) {
   const numbers = [5, 6, 7, 8, 9];
   const processedNumbers = processArray(numbers);
   console.log(processedNumbers); // Output will be [15, 36, 21, 64, 27]
+
+  function formatArrayStrings(strings, numbers) {
+    return strings.map((str, index) => {
+      if (numbers[index] % 2 === 0) {
+        return str.toUpperCase();
+      } else {
+        return str.toLowerCase();
+      }
+    });
+  }
+  const stringArray = ['the ', 'best country ', 'in the world'];
+  const numberArray = [3, 6, 9]; // Assuming this is the output from processArray
+  const formattedArray = formatArrayStrings(stringArray, numberArray);
+  console.log(formattedArray); // Output: ['the ', 'BEST COUNTRY ', 'in the world']
